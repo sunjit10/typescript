db49b3ada44472d6177d12a59916fa567d0487a9
Sunjit Tara <sunjit@gmail.com>
2025-01-15 20:16:30 -0600

    Arrays - initial version

---
+++ arrays/arrays-basics.ts
@@ -0,0 +1,10 @@
+// Arrays allow only data of one type within them
+
+// Syntax 1
+let names: string[] = ["hello", "world"];
+let ages: number[] = [10,30,40,20,70,60];
+
+// Syntax 2
+let cities: Array<string> = ["NYC", "SFO", "PHX"];
+let temps: Array<number> = [80,85,45,95];
+
---
+++ arrays/arrays-empty.ts
@@ -0,0 +1,7 @@
+let seasons: [] = [];
+
+// Cannot define array with value, it has to be empty
+// let seasons: [] = ["Spring"];
+
+// Since seasons is an empty array, it cannot have any value
+//seasons.push("Summer");
