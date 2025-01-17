23529e343a1b013cd38d2db7beaedcbb00574594
Sunjit Tara <sunjit@gmail.com>
2025-01-16 21:58:26 -0600

    Another array custom type

---
+++ arrays/arrays-customtype2.ts
@@ -0,0 +1,24 @@
+let ages: number[] = [];
+
+let gameBoard: string[][] = [[]];
+
+type Product = {
+ name: string;
+ price: number;
+};
+
+
+function getTotal(products: Product[]) :number {
+  let sum = 0;
+  for (let product of products) {
+    sum += product.price;
+  }
+  return sum;
+}
+
+let product1: Product = {name: "A", price:20};
+let product2: Product = {name: "B", price:30};
+
+
+let total = getTotal([product1, product2]);
+console.log(total);
